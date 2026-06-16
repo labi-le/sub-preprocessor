8b8fc7563fea2cb0df7d0ebfd0fae84e6fa94015
labi-le <1a6i1e@gmail.com>
2026-06-16 04:19:34 +0300

    add test worker

--- scripts/asn.sh
+++ scripts/asn.sh
@@ -7,8 +7,15 @@ rev=$(echo "$ip" | awk -F. '{print $4"."$3"."$2"."$1}')
 origin=$(dig +short TXT "${rev}.origin.asn.cymru.com" | tr -d '"')
 asn=$(echo "$origin" | awk -F'|' '{gsub(/ /,""); print $1}')
 as=$(dig +short TXT "AS${asn}.asn.cymru.com" | tr -d '"')
-name=$(echo "$as" | awk -F'|' '{print $5}' | sed 's/^[[:space:]]*//')
+name_full=$(echo "$as" | awk -F'|' '{print $5}' | sed 's/^[[:space:]]*//')
+name_short=$(echo "$name_full" | awk '{print $1}')
 
-echo "IP:      $ip"
-echo "Origin:  $origin"
-echo "AS:      AS${asn} — ${name}"
+cat <<EOF
+{
+  "ip": "$ip",
+  "asn": "$asn",
+  "as_name": "$name_full",
+  "as_name_short": "$name_short",
+  "origin": "$origin"
+}
+EOF
